9671cacb7141db77d65fcf233c2bab0c4d7d871a
nelson <pmfbighead@gmail.com>
2018-04-05 20:27:49 +0800

    6

--- src/main/java/com/example/springboot002/Springboot002Application.java
+++ src/main/java/com/example/springboot002/Springboot002Application.java
@@ -1,12 +1,25 @@
 package com.example.springboot002;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class Springboot002Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springboot002Application.class, args);
+		//SpringApplication.run(Springboot002Application.class, args);
+		SpringApplication app = new SpringApplication(Springboot002Application.class);
+		app.setBanner(new Banner() {
+						  @Override
+						  public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+						    out.print("\n  WOW Banner!!!\n \n");
+						  }
+					  }
+		);
+		app.run(args);
 	}
 }
